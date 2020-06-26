f31473ce051b9b37cf12da653b7d841d4e226efb
Michaela Cromie <michaelacromie@michaelas-mbp.home>
2020-06-26 14:12:47 +0100

    connecting redux to a component

---
+++ src/actions/logActions.js
@@ -0,0 +1,40 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
+
+// export const getLogs = () => {
+//     return async dispatch => {
+//         setLoading();
+
+//         const res = await fetch('/logs');
+//         const data = res.json();
+//         dispatch({
+//             type:GET_LOGS,
+//             payload: data
+//         });
+//     }
+// }
+
+//get logs from server
+export const getLogs = () => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch("/logs");
+    const data = res.json();
+    dispatch({
+      type: GET_LOGS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data,
+    });
+  }
+};
+
+//set loading to true
+export const setLoading = () => {
+  return {
+    type: SET_LOADING,
+  };
+};
