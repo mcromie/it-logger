0d129355594eb8b99008f871f24303392e422fc0
Michaela Cromie <michaelacromie@michaelas-mbp.home>
2020-06-26 13:33:19 +0100

    Edit and tech modal

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-export const AddLogModal = () => {
+export const EditLogModal = () => {
 
     const [message, setMessage ] = useState('');
     const [attention, setAttention] = useState(false);
@@ -12,16 +12,17 @@ export const AddLogModal = () => {
             M.toast({ html: 'Please enter a message and a tech'})
         } else{
             console.log(message, tech, attention)
-              //clear fields
-              setMessage('');
-              setTech('');
-              setAttention(false);
+            //clear fields
+            setMessage('');
+            setTech('');
+            setAttention(false);
         }
+
         
     }
 
     return (
-        <div id="add-log-modal" className="modal" style={modalStyle}>
+        <div id="edit-log-modal" className="modal" style={modalStyle}>
            <div className="modal-content">
                <h4>Enter System Log</h4>
                <div className="row">
